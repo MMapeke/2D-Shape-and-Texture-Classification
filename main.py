1e1b825f53287009ca501e5cbb6c74b4bd88e6b1
Marc Mapeke <marc_mapeke@brown.edu>
2021-05-09 17:05:02 -0400

    working generation code

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
+import os 
 import texture_gen
+import glob 
+
 from generate_shapes import ShapeGenerator
+from skimage import io, color, img_as_float32
 
 def main():
     # Generate 2d Shape Images
     # DATASETS: 3 Shapes, 5 Shapes, 5 Shapes w/ Random Size
 
-    num_images = 2 # Number of images per each shape
+    num_images = 350 # Number of images per each shape
     # Destination path for original 2D images
-    destination_orig = "./datasets/three_shapes/original" 
+    destination_orig = "./datasets/three_shapes/original/" 
     print("Generating Images and saving to " + destination_orig)
 
-    generator = ShapeGenerator(destination, num_images)
+    generator = ShapeGenerator(destination_orig, num_images)
     generator.generate_shapes()
     
     # Apply Textures to Images
-    destination_textured = "./datasets/three_shapes/textured" 
+    # NOTE: destination_textured should end in '/'
+    destination_textured = "./datasets/three_shapes/textured/" 
+    print("Generating Texutes and saving to " + destination_textured)
     textures, t_names = texture_gen.load_textures()
 
     # Load original image shapes + generate new images to save
@@ -25,10 +31,11 @@ def main():
         # print(img.shape)
         img = img_as_float32(img)
         
-        texture_gen.generate_textures(img, textures)
+        texture_gen.generate_textures(img, f1, textures, t_names, destination_textured)
 
     # TODO: Learning and Classification
      
 
+# TODO: next commit datasets in zip + all the code
 if __name__ == '__main__':
     main()
--- texture_gen.py
+++ texture_gen.py
@@ -1,8 +1,10 @@
 import cv2 
 import os 
 import glob 
-from skimage import io, color, img_as_float32
+from skimage import io, color, img_as_float32, img_as_ubyte
 import numpy as np
+from PIL import Image
+import uuid
 
 # Loads and resizes textures
 def load_textures():
@@ -10,21 +12,25 @@ def load_textures():
     img_dir = "./textures" # Directory for Textures
     data_path = os.path.join(img_dir,'*g') 
     files = glob.glob(data_path) 
-    textures = [] 
+    textures = []
+    t_names = [] 
     for f1 in files: 
         img = io.imread(f1, as_gray = True)
          # Resizes Textures to be 200 by 200
         img = cv2.resize(img, dsize = (200,200))
         img = img_as_float32(img)
-        textures.append(img) 
+        textures.append(img)
 
-    # Returns list of textures
-    return textures
-    # TODO: Return second array of texture names, so making files simple later
+        # Parse texture name
+        texture_name = (f1.split('\\')[-1]).split('.')[0]
+        t_names.append(texture_name) 
 
-# Generates and Returns Shapes w/ Texture (as np arrays)
-def generate_textures(img1):
-    textures = load_textures()
+    # Returns list of textures + associated names
+    return textures, t_names
+
+# Generates and Saves Shape w/ Texture Applied
+# Assumes original image is black background, white foreground
+def generate_textures(img1,img_name,textures,t_names,dest_textured):
     textured_images = []
 
     for texture in textures:
@@ -32,6 +38,18 @@ def generate_textures(img1):
         img2 = img1 + texture 
         # Compare orig texture and img2, where not equal is our shape pixels
         new_image = np.where((img2 == texture),0.5,texture)
-        textured_images.append(new_image)
+
+        textured_images.append(img_as_ubyte(new_image))
     
-    return textured_images
+    save_images(textured_images, img_name, t_names, dest_textured)
+
+# Saves textured images, based off original image name and textured image names
+def save_images(images, img_name, t_names, dest):    
+    # Find Shape Name
+    shape = img_name.split('\\')[-1].split('_')[0]
+
+    for img,texture in zip(images,t_names):
+        # Generate Name w/ UUID
+        filename  = shape + "_" + texture + "_" + str(uuid.uuid1()) + ".png"
+        # Save Image in Dest
+        Image.fromarray(img).convert("L").save(dest + filename)
