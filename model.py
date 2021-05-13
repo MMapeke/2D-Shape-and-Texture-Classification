1da1b669e263537343634e4a71fd7b8b5edeb1e2
Marc Mapeke <marc_mapeke@brown.edu>
2021-05-13 16:20:16 -0400

    tuning model params

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ img_height = img_width = 200
 batch_size = 15
 
 # Use dataset from directory
-directory = "./datasets/three_shapes_t/"
+directory = "./datasets/three_shapes_size_t/"
 
 ds_train = tf.keras.preprocessing.image_dataset_from_directory(
     directory, 
@@ -24,7 +24,7 @@ ds_train = tf.keras.preprocessing.image_dataset_from_directory(
     image_size = (img_height, img_width),
     shuffle = True,
     seed = 123,
-    validation_split = 0.25,
+    validation_split = 0.2,
     subset = "training"
 )
 
@@ -37,7 +37,7 @@ ds_validation = tf.keras.preprocessing.image_dataset_from_directory(
     image_size = (img_height, img_width),
     shuffle = True,
     seed = 123,
-    validation_split = 0.25,
+    validation_split = 0.2,
     subset = "validation"
 )
 
@@ -46,6 +46,7 @@ class_names = ds_train.class_names
 
 num_classes = len(class_names)
 
+# Custom Model
 model = keras.Sequential([
   layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 1)),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
@@ -55,8 +56,9 @@ model = keras.Sequential([
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Flatten(),
+  layers.Dropout(0.2),
   layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+  layers.Dense(num_classes, activation="softmax")
 ])
 
 model.compile(optimizer=keras.optimizers.Adam(),
@@ -65,7 +67,7 @@ model.compile(optimizer=keras.optimizers.Adam(),
 
 model.summary()
 
-num_epochs = 7
+num_epochs = 10
 history = model.fit(
   ds_train,
   epochs=num_epochs,
@@ -95,6 +97,24 @@ plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
 
-# TODO: also uh accuracy wayy too high so idk if workings + look into additional
-# metrics for performace
-# visualize training reuslts part of google thing
+
+# TODO : 
+# play with custom model + regularization type stuff, then transfer learning with other classic models
+# look into visualizing misclassified + other accuracy metrics
+
+# Once this is done try super large like 7 shapes type dataset
+# Evaluate the model on the test data using `evaluate`
+test_directory = "./datasets/test_size_t/"
+
+ds_test = tf.keras.preprocessing.image_dataset_from_directory(
+    test_directory, 
+    labels = "inferred",
+    label_mode = "int",
+    color_mode = "grayscale",
+    batch_size = batch_size,
+    image_size = (img_height, img_width),
+)
+
+print("Evaluate on test data")
+results = model.evaluate(ds_test)
+print("test loss, test acc:", results)
